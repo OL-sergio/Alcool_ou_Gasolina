e46c80ee9a24002ac89df51d85dcbdb9d91bfb19
Sérgio <a18074@alunos.ipca.pt>
2022-08-16 22:55:35 +0100

    Initial commit

---
+++ app/src/main/java/udemy/java/alcoolougasolina/MainActivity.java
@@ -0,0 +1,14 @@
+package udemy.java.alcoolougasolina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
